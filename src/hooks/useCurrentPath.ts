f46533a2be89eac788f025bc75129470c43fc7b5
Sebastian <salvarezocampo@hotmail.com>
2022-07-10 16:17:29 -0500

    Fixing my hook to use the route i want

--- src/hooks/useCurrentPath.ts
+++ src/hooks/useCurrentPath.ts
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 const useCurrentPath = () => {
 
     const lcoation = useLocation();
-    return location.pathname === '/';
+    return location.pathname === '/breaking-bad-app';
 }
 
 export default useCurrentPath;
